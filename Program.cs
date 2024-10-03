f5087f875b1c19685f0d3f6245ecb5ba1a5b168b
s28328 <s28328@pjwstk.edu.pl>
2024-10-03 13:25:39 +0200

    Added AppDbContext and configured ConnectionString

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Pet_project.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => 
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    );
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
